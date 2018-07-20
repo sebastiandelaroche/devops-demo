fac43c8956bf17836ed023c8716b7ac3d42b7b8f
sebastianroche <sebastian@softdev.team>
2018-07-20 02:01:13 -0500

    ..

--- .circleci/deployments/prod.sh
+++ .circleci/deployments/prod.sh
@@ -1,15 +1,9 @@
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
 unzip /tmp/terraform.zip -d ~/bin
 
-ls
-
-echo $HOME
-echo $GOOGLE_AUTH
-
 echo $GOOGLE_AUTH > $HOME/gcp-key.json
 gcloud auth activate-service-account --key-file $HOME/gcp-key.json
 gcloud --quiet config set project $GCP_PROJECT
-gcloud docker -- push gcr.io/$GCP_PROJECT/$IMAGE_NAME:$CIRCLE_SHA1
 
 cd infra && terraform init -input=false &&
 terraform apply -input=false -auto-approve
