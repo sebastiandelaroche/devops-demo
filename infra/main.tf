3f9399231b275568a55fc04329b4274128bee828
sebastianroche <sebastian@softdev.team>
2018-07-20 01:25:17 -0500

    terraform

--- infra/main.tf
+++ infra/main.tf
@@ -4,40 +4,40 @@ provider "kubernetes" {
   password = "9AlygNOPh6dCFFXW"
 }
 
-resource "kubernetes_pod" "demo-service" {
+resource "kubernetes_service" "example" {
   metadata {
-    name = "demo"
-
-    labels {
-      app = "demo-service"
-    }
-  }
-
-  spec {
-    container {
-      image = "gcr.io/development-207315/demo-service:2795f9564962ba21197b49eb0ceca9e486f7e2f5"
-      name  = "demo-service"
-    }
-  }
-}
-
-resource "kubernetes_service" "demo-service" {
-  metadata {
-    name = "demo"
+    name = "terraform-example"
   }
 
   spec {
     selector {
-      app = "${kubernetes_pod.demo-service.metadata.0.labels.app}"
+      app = "${kubernetes_pod.example.metadata.0.labels.app}"
     }
 
     session_affinity = "ClientIP"
 
     port {
       port        = 8080
-      target_port = 3000
+      target_port = 80
     }
 
     type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_pod" "example" {
+  metadata {
+    name = "terraform-example"
+
+    labels {
+      app = "MyApp"
+    }
+  }
+
+  spec {
+    container {
+      image = "gcr.io/development-207315/demo-service:2795f9564962ba21197b49eb0ceca9e486f7e2f5"
+      name  = "example"
+    }
   }
 }
