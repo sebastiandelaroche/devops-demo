a18c8bfea21ac58abcf11fa13974196c2346b6f6
sebastianroche <sebastian@softdev.team>
2018-07-20 00:22:03 -0500

    ...

--- infra/main.tf
+++ infra/main.tf
@@ -3,3 +3,16 @@ provider "kubernetes" {
   username = "admin"
   password = "9AlygNOPh6dCFFXW"
 }
+
+resource "kubernetes_pod" "demo-service" {
+  metadata {
+    name = "demo"
+  }
+
+  spec {
+    container {
+      image = "gcr.io/development-207315/demo-service:2795f9564962ba21197b49eb0ceca9e486f7e2f5"
+      name  = "demo-service"
+    }
+  }
+}
