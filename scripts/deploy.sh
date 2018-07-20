5173aaa6d00074faebd5f48588c983bd94b2993b
sebastianroche <sebastian@softdev.team>
2018-07-20 01:41:08 -0500

    ..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,1 @@
-    wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip \
-    unzip /tmp/terraform.zip -d ~/bin \
-    cd infra && terraform init -input=false && \
-    terraform apply -input=false -auto-approve
+ls
