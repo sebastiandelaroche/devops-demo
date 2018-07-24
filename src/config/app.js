2d87e86515bac0d6b86129d5b99b3b8321914cb1
sebastianroche <sebastian@softdev.team>
2018-07-23 23:08:32 -0500

    little change

--- src/config/app.js
+++ src/config/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi world from from kubernetes !!!!kadbasjdbajkdb!');
+    res.send('Hi world from kubernetes developed by Sebastian and Mateo!');
 });
 
 module.exports = app;
